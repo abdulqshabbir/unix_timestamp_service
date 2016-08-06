c9a21f94679326de3e6451edd16489142480b47c
Abdul Shabbir <abdulqshabbir@gmail.com>
2016-08-06 13:25:58 -0400

    Added matching for 'unix time stamps' and 'regular time stamps'

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 //require express and instantiate app
 var express = require('express');
 var app = express();
+var naturalTimeMatch = /\w+\s?\d{1,2},\s?\d{4}/;
+var unixTimeMatch = /^\d+/;
 
-//home directory
+
+//home directory listing instructions
 app.get('/', function (req, res) {
     res.send('Please type in a date in the address bar to recieve your Unix Timestamp!');
 });
@@ -10,11 +13,23 @@ app.get('/', function (req, res) {
 //get time from user
 app.get('/:time', function (req, res) {
     var time = req.params.time;
-    res.send(time);
-    console.log(time);
+    if(time.match(naturalTimeMatch)) {
+        console.log('natural time match!');
+        
+    }
+
+    else if(time.match(unixTimeMatch)) {
+        console.log('unix tiem match!');
+    }
+    else {
+        res.json({unix: null, naturalTime: null});
+    }
 });
 
 //listen on port 3000
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
+
+//getTime returns the number of milliseconds since January 01, 1970
+console.log(new Date('1969, December, 31').getTime());

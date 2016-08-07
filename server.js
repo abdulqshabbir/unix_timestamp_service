c37ef8b622476d5953059de7d15e11d6b953f982
Abdul Shabbir <abdulqshabbir@gmail.com>
2016-08-07 14:15:07 -0400

    Intialized Procfile for Heroku Deployment

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 //require express and instantiate app
 var express = require('express');
 var app = express();
-var naturalTimeMatch = /\w+\s?\d{1,2},\s?\d{4}/;
-var unixTimeMatch = /^\d+/;
+var naturalTime = /\w+\s?\d{1,2},\s?\d{4}/;
+var unixTime = /^\d+/;
 
 
 //home directory listing instructions
@@ -13,23 +13,26 @@ app.get('/', function (req, res) {
 //get time from user
 app.get('/:time', function (req, res) {
     var time = req.params.time;
-    if(time.match(naturalTimeMatch)) {
-        console.log('natural time match!');
-        
+    if(time.match(naturalTime)) {
+        var naturalToUnix = new Date(time).getTime()/1000;
+        res.json({'unix': naturalToUnix, 'naturalTime': time});
+
     }
 
-    else if(time.match(unixTimeMatch)) {
-        console.log('unix tiem match!');
+    else if(time.match(unixTime)) {
+        var unixToNatural = new Date(+time*1000);
+        res.json({'unix': +time, 'naturalTime': unixToNatural});
     }
     else {
         res.json({unix: null, naturalTime: null});
     }
 });
 
+var port = Number(process.env.PORT || 3000);
+
 //listen on port 3000
-app.listen(3000, function () {
+app.listen(port, function () {
   console.log('Example app listening on port 3000!');
 });
 
 //getTime returns the number of milliseconds since January 01, 1970
-console.log(new Date('1969, December, 31').getTime());
